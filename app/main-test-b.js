859d53ae86c9af95e858f9851494cfe35afc25c7
“pangff” <admin@example.com>
2017-03-08 15:30:16 +0800

    first init

---
+++ app/main-test-b.js
@@ -0,0 +1,9 @@
+"use strict";
+import test from "./test.js"
+
+
+$(document).ready(function() {
+    document.getElementById("test").innerHTML=test;
+    console.log(test)
+});
+
---
+++ app/test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by pangff on 17/3/7.
+ */
+"use strict";
+
+let a = "gulp 测试";
+
+export default a;
